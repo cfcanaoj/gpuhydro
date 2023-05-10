c9c7fdbaee6fbb125351bf5d07a93cb6a809e3b6
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2023-05-10 16:35:15 +0900

    updater

--- KHf90openacc/sj_g00.sh
+++ KHf90openacc/sj_g00.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-#SBATCH --partition=ga80-1gpu
+#SBATCH --partition=dgx-full
 #SBATCH --ntasks=4
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 
 # usage sbatch sj_ori.sh
 # other useful commands
