6a6ef23738494d77cf17c8552b96598b7604b003
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2023-05-11 16:19:45 +0900

    update

--- DTf90openacc/sj_g00.sh
+++ DTf90openacc/sj_g00.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 #SBATCH --partition=ga80-1gpu
 #SBATCH --gres=gpu:1
+#SBATCH -o ./out%j.log
+#SBATCH -e ./err%j.log
 
 # usage sbatch sj_g00.sh
 # other useful commands
@@ -8,4 +10,4 @@
 # squeue
 
 module load nvhpc
-./Simulation.x > log.dat
+./Simulation.x
